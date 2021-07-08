f1369ac946fa0006527878a54d7aebf4593e1b5f
Rudge Renan Ferreira <rudge.ferreira@c6bank.com>
2021-07-08 20:13:35 -0300

    test: Return object in response

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+class Token:
+    def __init__(self, access_token: str, token_type: str):
+         self.access_token = access_token
+         self.token_type = token_type
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return Token(access_token="access", token_type="token")
